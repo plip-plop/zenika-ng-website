48bcd371d941067050749bfda50c5285392911dd
unknown <emmanuel.kerjean@gmail.com>
2023-11-28 14:51:53 +0100

    tp3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ProductComponent } from './components/product/product.component';
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    component.products = [{
+      id: 'ID',
+      title: 'TITLE',
+      description: 'DESC',
+      photo: 'PHOTO',
+      price: 10,
+      stock: 2,
+    }];
+
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'zenika-ng-website'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('zenika-ng-website');
+  it('It should display the products', () => {
+    const productElement = fixture.debugElement.query(By.css('app-product'));
+    expect(productElement.properties['product']).toBe(component.products[0]);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('It should update the total when "addToBasket" class method is called (Class testing)', () => {
+    component.total = 99;
+    const productElement = fixture.debugElement.query(By.css('app-product'));
+    productElement.triggerEventHandler('addToBasket', component.products[0]);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zenika-ng-website app is running!');
+    
+    const header = (fixture.nativeElement as HTMLElement).querySelector('header')?.querySelector('h1');
+    expect(header?.textContent).toBe('Bienvenue sur Zenika Ecommerce');
+  });
+
+  it('It should update the total when a product emits the "addToBasket" event (DOM testing)', () => {
+    component.total = 99;
+    component.ajouterAuPanier(component.products[0]);
+    expect(component.total).toBe(99 + component.products[0].price);
   });
 });
--- src/app/components/product/product.component.spec.ts
+++ src/app/components/product/product.component.spec.ts
@@ -2,20 +2,59 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
 
+
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductComponent]
+      declarations: [ProductComponent],
     });
+
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+
+    // Define the required `product` property of the component
+    component.product = {
+      id: 'ID',
+      title: 'TITLE',
+      description: 'DESC',
+      photo: 'PHOTO',
+      price: 10,
+      stock: 2,
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('It should display the product photo as image url', () => {
+    const img = fixture.nativeElement.querySelector('img');
+    expect(img.src).toContain(component.product.photo);
+  });
+
+  it('It should display the product description', () => {
+    const small = fixture.nativeElement.querySelector('small');
+    expect(small.textContent).toBe(component.product.description);
+  });
+
+  it('It should display the product title', () => {
+    const title = (fixture.nativeElement as HTMLElement).querySelector('.card-link');
+    expect(title?.textContent).toBe(component.product.title);
+  });
+
+  it('It should display the product price', () => {
+    const title = (fixture.nativeElement as HTMLElement).querySelector('.card-text');
+    expect(title?.textContent).toContain(component.product.price);
+  });
+
+  it('It should emit addToBasket event with the given product when the button is clicked', () => {
+    const emitSpy = spyOn(component.addToBasket, 'emit');
+    (fixture.nativeElement as HTMLElement).querySelector('button')?.click();
+    expect(emitSpy).toHaveBeenCalledOnceWith(component.product);
   });
 });
