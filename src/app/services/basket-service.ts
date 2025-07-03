712214962487a6e328e9cb6652569864cf3b13c1
unknown <emmanuel.kerjean@gmail.com>
2025-07-03 16:16:42 +0200

    TP8

--- src/app/services/basket-service.ts
+++ src/app/services/basket-service.ts
@@ -7,6 +7,7 @@ import { BasketItem } from './basket-item';
 export class BasketService {
   _items = signal<BasketItem[]>([]);
   items = this._items.asReadonly();
+
   total = computed<number>(() =>
     this._items().reduce((previous, next) => previous + next.price, 0)
   );
